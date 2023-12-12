4b8007bb4569040a9f5511b7ac9b734654f82777
gyanlearning <granlearning12@gmail.com>
2023-12-13 01:59:19 +0530

    add some file

---
+++ scripts/after_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+
+echo 'cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.loh
+
+echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
